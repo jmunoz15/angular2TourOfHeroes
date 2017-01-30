f91be571ba789e02707270e9739f759c5ee4f4cd
Julio Cesar Munoz Solis <julio.munoz@avantica.net>
2017-01-30 16:48:15 -0600

    Initial commit for the Forms Project

--- angular-tour-of-heroes/app/hero-search.service.ts
+++ angular-tour-of-heroes/app/hero-search.service.ts
@@ -6,6 +6,7 @@ import { Hero }           from './hero';
 @Injectable()
 export class HeroSearchService {
   constructor(private http: Http) {}
+  
   search(term: string): Observable<Hero[]> {
     return this.http
                .get(`app/heroes/?name=${term}`)
